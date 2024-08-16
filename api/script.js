cc09331cee21bc2e36fe3c67ed4e21273dc4bd9b
Sounava Bhowmick <158716010+realpega@users.noreply.github.com>
2024-08-16 21:28:06 +0530

    Update script.js

--- api/script.js
+++ api/script.js
@@ -1,6 +1,8 @@
+const express = require('express');
 const axios = require('axios');
+const app = express();
 
-module.exports = async (req, res) => {
+app.get('/api/freefire', async (req, res) => {
     const { region, uid } = req.query;
     const apiUrl = `https://www.info.freefireinfo.site/api/${region}/${uid}?key=sounava777`;
 
@@ -11,4 +13,9 @@ module.exports = async (req, res) => {
         console.error(error);
         res.status(500).json({ error: 'Failed to fetch data' });
     }
-};
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
