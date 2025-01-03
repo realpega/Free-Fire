18520a29e724affbb6d126360c93f61422e0ea38
Sounava Bhowmick <158716010+realpega@users.noreply.github.com>
2025-01-03 19:57:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,10 +26,60 @@ function unixToHumanTime(unixTime) {
 }
 
 function getPetName(petId) {
-    if (petId === "1300000112") {
+    if (petId === "1300000001") {
         return "Kitty";
+    } else if (petId === "1300000021") {
+            return "Night Panther";
     } else if (petId === "1300000031") {
-        return "Panda";
+            return "Detective Panda";
+    } else if (petId === "1300000041") {
+            return "Shiba";
+    } else if (petId === "1300000031") {
+            return "Detective Panda";
+    } else if (petId === "1300000051") {
+            return "Spirit Fox";
+    } else if (petId === "1300000061") {
+            return "Robo";
+    } else if (petId === "1300000071") {
+            return "Poring";
+    } else if (petId === "1300000081") {
+            return "Ottero";
+    } else if (petId === "1300000091") {
+            return "Falco";
+    } else if (petId === "1300000101") {
+            return "Mr. Waggor";
+    } else if (petId === "1300000031") {
+            return "Detective Panda";
+    } else if (petId === "1300000111") {
+            return "Rockie";
+    } else if (petId === "1300000112") {
+            return "Beaston";
+    } else if (petId === "1300000113") {
+            return "Dreki";
+    } else if (petId === "1300000114") {
+            return "Moony";
+    } else if (petId === "1300000115") {
+            return "Dr. Beanie";
+    } else if (petId === "1300000116") {
+            return "Sensei Tig";
+    } else if (petId === "1300000117") {
+            return "Agent Hop";
+    } else if (petId === "1300000118") {
+            return "Yeti";
+    } else if (petId === "1300000119") {
+            return "Flash";
+    } else if (petId === "1300000120") {
+            return "Zasil";
+    } else if (petId === "1300000121") {
+            return "Finn";
+    } else if (petId === "1300000123") {
+            return "Fang";
+    } else if (petId === "1300000124") {
+            return "Arvon";
+    } else if (petId === "1300000125") {
+            return "Kactus";
+    } else if (petId === "1300000126") {
+            return "Pug";
     } else {
         return "";
     }
@@ -51,13 +101,12 @@ function getPetName(petId) {
         document.getElementById('liked').textContent = data.basicInfo.liked;
         document.getElementById('title').textContent = data.basicInfo.title;
 
-                const petName1 = getPetName(data.petInfo.id);
-    if (petName1) {
-        document.getElementById('petId').textContent = petName1;
-    } else {
-        document.getElementById('petId').textContent = data.petInfo.id;
-    }
-        //document.getElementById('petId').textContent = data.petInfo.id;
+        const petNickname = getPetName(data.petInfo.id);
+        if (petNickname) {
+                document.getElementById('petId').textContent = petNickname;
+        } else {
+                document.getElementById('petId').textContent = data.petInfo.id;
+        }
         document.getElementById('petName').textContent = data.petInfo.name;
         document.getElementById('petStatus').textContent = data.petInfo.isSelected;
         document.getElementById('petLevel').textContent = data.petInfo.level;
