c8a41f9e70fdde848fc8e5b82abd42776c461096
Sounava Bhowmick <158716010+realpega@users.noreply.github.com>
2025-01-04 14:39:43 +0530

    Create pet.js

---
+++ pet.js
@@ -0,0 +1,59 @@
+function getPetName(petId) {
+    if (petId === "1300000001") {
+        return "Kitty";
+    } else if (petId === "1300000021") {
+            return "Night Panther";
+    } else if (petId === "1300000031") {
+            return "Detective Panda";
+    } else if (petId === "1300000041") {
+            return "Shiba";
+    } else if (petId === "1300000031") {
+            return "Detective Panda";
+    } else if (petId === "1300000051") {
+            return "Spirit Fox";
+    } else if (petId === "1300000061") {
+            return "Robo";
+    } else if (petId === "1300000071") {
+            return "Poring";
+    } else if (petId === "1300000081") {
+            return "Ottero";
+    } else if (petId === "1300000091") {
+            return "Falco";
+    } else if (petId === "1300000101") {
+            return "Mr. Waggor";
+    } else if (petId === "1300000031") {
+            return "Detective Panda";
+    } else if (petId === "1300000111") {
+            return "Rockie";
+    } else if (petId === "1300000112") {
+            return "Beaston";
+    } else if (petId === "1300000113") {
+            return "Dreki";
+    } else if (petId === "1300000114") {
+            return "Moony";
+    } else if (petId === "1300000115") {
+            return "Dr. Beanie";
+    } else if (petId === "1300000116") {
+            return "Sensei Tig";
+    } else if (petId === "1300000117") {
+            return "Agent Hop";
+    } else if (petId === "1300000118") {
+            return "Yeti";
+    } else if (petId === "1300000119") {
+            return "Flash";
+    } else if (petId === "1300000120") {
+            return "Zasil";
+    } else if (petId === "1300000121") {
+            return "Finn";
+    } else if (petId === "1300000123") {
+            return "Fang";
+    } else if (petId === "1300000124") {
+            return "Arvon";
+    } else if (petId === "1300000125") {
+            return "Kactus";
+    } else if (petId === "1300000126") {
+            return "Pug";
+    } else {
+        return "";
+    }
+}
