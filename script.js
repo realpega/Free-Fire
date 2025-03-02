badfa8569fe91273606fdff694e23e98ce954ecb
Sounava Bhowmick <158716010+realpega@users.noreply.github.com>
2025-03-02 07:07:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function setRegion(newRegion) {
 
 function redirectToURL() {
         const uid = document.getElementById("uid").value;
-        const key = "83848373993037";
+        const key = "march-2";
         const url = `https://free-fire-data.vercel.app/api/data?region=${region}&uid=${uid}&key=${key}`;        
         fetch(url)
                 .then(response => response.json())
